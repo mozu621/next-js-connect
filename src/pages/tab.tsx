35cce030688ce56976d337dbedf89185596dcdd7
Kazuya Mori <kazuya.mori0621@gmail.com>
2022-07-07 05:33:07 +0900

    create tab2

--- src/pages/tab.tsx
+++ src/pages/tab.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Tabs = () => {
   const [openTab, setOpenTab] = React.useState(1);
+  const [inputValue, setInputValue] = useState('');
+  const [inputValue2, setInputValue2] = useState('');
+  const handleChange2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleChange3 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue2(e.target.value);
+  };
   return (
     <>
       <div className='flex flex-wrap'>
@@ -21,7 +31,7 @@ const Tabs = () => {
                 href='#link1'
                 role='tablist'
               >
-                Profile
+                Textsearch
               </a>
             </li>
             <li className='flex-auto mr-2 last:mr-0 -mb-px text-center'>
@@ -38,7 +48,7 @@ const Tabs = () => {
                 href='#link2'
                 role='tablist'
               >
-                Settings
+                Tabsearch
               </a>
             </li>
           </ul>
@@ -46,23 +56,22 @@ const Tabs = () => {
             <div className='flex-auto py-5 px-4'>
               <div className='tab-content tab-space'>
                 <div className={openTab === 1 ? 'block' : 'hidden'} id='link1'>
-                  <p>
-                    Collaboratively administrate empowered markets via plug-and-play networks.
-                    Dynamically procrastinate B2C users after installed base benefits.
-                    <br />
-                    <br /> Dramatically visualize customer directed convergence without
-                    revolutionary ROI.
-                  </p>
+                  <input
+                    type='text'
+                    value={inputValue}
+                    onChange={handleChange2}
+                    className='py-3 pr-4 pl-10 w-full text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-800 rounded-md border focus:border-blue-500 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring'
+                    placeholder='タイトルで検索'
+                  />
                 </div>
                 <div className={openTab === 2 ? 'block' : 'hidden'} id='link2'>
-                  <p>
-                    Completely synergize resource taxing relationships via premier niche markets.
-                    Professionally cultivate one-to-one customer service with robust ideas.
-                    <br />
-                    <br />
-                    Dynamically innovate resource-leveling customer service for state of the art
-                    customer service.
-                  </p>
+                  <input
+                    type='text'
+                    value={inputValue}
+                    onChange={handleChange3}
+                    className='py-3 pr-4 pl-10 w-full text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-800 rounded-md border focus:border-blue-500 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring'
+                    placeholder='tabで検索'
+                  />
                 </div>
               </div>
             </div>
